91d941f4a5fe6d1db4a7538e1ad5a94d8e193dcf
Tinnaphop Choonchuachan <53388199+mistertfy64@users.noreply.github.com>
2022-07-07 19:02:57 +0700

    change: added leaderboard ranks to user api

--- server/api/users.js
+++ server/api/users.js
@@ -8,11 +8,28 @@ const limiter = rateLimit({
     legacyHeaders: false
 });
 
+const _ = require("lodash");
+
 const User = require("../models/User.js");
+const EasyModeLeaderboardsRecord = require("../models/EasyModeLeaderboardsRecord.js");
+const StandardModeLeaderboardsRecord = require("../models/StandardModeLeaderboardsRecord.js");
 
 router.get("/api/users/:username", limiter, async (request, response) => {
-    if (!/[A-Za-z0-9_]{3,32}/.test(username)) { response.status(400).json("Invalid Request.") }
-    let data = await User.safeFindByUsername(mongoDBSanitize(request.params.username));
+    if (!request?.params?.username) { response.status(400).json("Invalid Request."); return; }
+    let username = request.params.username;
+    if (!/[A-Za-z0-9_]{3,32}/.test(username)) { response.status(400).json("Invalid Request."); return; }
+    let data = await User.safeFindByUsername(mongoDBSanitize.sanitize(username));
+    data = JSON.parse(JSON.stringify(data));
+    if (data == null) {response.status(400).json("Invalid Request."); return;}
+    let easyLeaderboardData = await EasyModeLeaderboardsRecord.findOne({userIDOfHolder: data._id.toString()});
+    let standardLeaderboardData = await StandardModeLeaderboardsRecord.findOne({userIDOfHolder: data._id.toString()});
+    // add leaderboards data
+    if (easyLeaderboardData != null){
+        data.statistics.personalBestScoreOnEasySingleplayerMode.globalRank = easyLeaderboardData.rankNumber; 
+    }
+    if (standardLeaderboardData != null){
+        data.statistics.personalBestScoreOnStandardSingleplayerMode.globalRank = standardLeaderboardData.rankNumber; 
+    }
     response.status(200).json(data);
 });
 
