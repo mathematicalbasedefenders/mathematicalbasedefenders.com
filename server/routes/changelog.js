953bd08314f1089eba04b3e53de4ad07baa62c13
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2022-09-03 10:13:26 +0700

    change: made `changelog` page get data from serverside instead of clientside

--- server/routes/changelog.js
+++ server/routes/changelog.js
@@ -6,8 +6,46 @@ const limiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false
 });
-router.get("/changelog", limiter,(request, response) => {
-    response.render("pages/changelog");
+const https = require("https");
+const marked = require("marked");
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+
+router.get("/changelog", limiter,async(request, response) => {
+    let data = {}
+    data.gameChangelog = md.render(await loadText("game"));
+    data.websiteChangelog = md.render(await loadText("website"));   
+    response.render("pages/changelog", {data: data});
 });
+
+async function loadText(service) {
+    let fileURL;
+    switch (service) {
+        case "game": {
+            fileURL =
+                "https://raw.githubusercontent.com/mathematicalbasedefenders/information/main/GAME_CHANGELOG.md";
+            break;
+        }
+        case "website": {
+            fileURL =
+                "https://raw.githubusercontent.com/mathematicalbasedefenders/information/main/WEBSITE_CHANGELOG.md";
+            break;
+        }
+        default: {
+            return "";
+        }
+    }
+    return new Promise(async (resolve, reject) => {
+        let data = "";
+        await https.get(fileURL, (response) => {
+            response.on("data", (chunk) => {
+                data += chunk.toString("utf-8");
+            });
+            response.on("end", function () {
+                resolve(marked.parse(data));
+            });
+        });
+    });
+}
 
 module.exports = router;
