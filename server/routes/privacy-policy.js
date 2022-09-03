0b4206e9e087f07a15aa5fd0e6b193d5272ea690
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2022-09-03 16:05:37 +0700

    change: made `privacy-policy` page load text from github server-sided now

--- server/routes/privacy-policy.js
+++ server/routes/privacy-policy.js
@@ -6,8 +6,25 @@ const limiter = rateLimit({
     standardHeaders: true,
     legacyHeaders: false
 });
-router.get("/privacy-policy", limiter, (request, response) => {
-    response.render("pages/privacy-policy");
-});
+const https = require("https");
+const marked = require("marked");
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt({html:true});
 
+router.get("/privacy-policy", limiter, async (request, response) => {
+    response.render("pages/privacy-policy",{data:{privacyPolicy:md.render(await loadPrivacyPolicy())}});
+});
+async function loadPrivacyPolicy() {
+    return new Promise(async (resolve, reject) => {
+        let data = "";
+        await https.get("https://raw.githubusercontent.com/mathematicalbasedefenders/information/main/PRIVACY_POLICY.md", (response) => {
+            response.on("data", (chunk) => {
+                data += chunk.toString("utf-8");
+            });
+            response.on("end", function () {
+                resolve(marked.parse(data));
+            });
+        });
+    });
+}
 module.exports = router;
