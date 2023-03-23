2841b7e796bf23cab4bc05d3440655acb40a86cd
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-03-23 22:02:40 +0700

    feature: add changelog page

---
+++ src/server/routes/changelog.ts
@@ -0,0 +1,75 @@
+import express from "express";
+var router = express.Router();
+import rateLimit from "express-rate-limit";
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false
+});
+import https from "https";
+import { marked } from "marked";
+
+interface ChangelogContent {
+  text: string;
+  part: string;
+}
+
+router.get("/changelog", limiter, async (request, response) => {
+  response.redirect("/changelog/game");
+});
+
+router.get("/changelog/:service", limiter, async (request, response) => {
+  let data = <ChangelogContent>{};
+  // data.gameChangelog = marked.parse((await loadText("game")) as string);
+  switch (request.params.service) {
+    case "website": {
+      data.text = marked.parse((await loadText("website")) as string);
+      data.part = "Website";
+      response.render("pages/changelog", { data: data });
+      return;
+    }
+    case "game": {
+      data.text = marked.parse((await loadText("game")) as string);
+      data.part = "Game";
+      response.render("pages/changelog", { data: data });
+      return;
+    }
+    default: {
+      response.redirect("/changelog/game");
+      return;
+    }
+  }
+});
+
+async function loadText(service: string) {
+  let fileURL: string;
+  switch (service) {
+    case "game": {
+      fileURL =
+        "https://raw.githubusercontent.com/mathematicalbasedefenders/information/main/GAME_CHANGELOG.md";
+      break;
+    }
+    case "website": {
+      fileURL =
+        "https://raw.githubusercontent.com/mathematicalbasedefenders/information/main/WEBSITE_CHANGELOG.md";
+      break;
+    }
+    default: {
+      return "";
+    }
+  }
+  return new Promise(async (resolve, reject) => {
+    let data = "";
+    await https.get(fileURL, (response) => {
+      response.on("data", (chunk) => {
+        data += chunk.toString("utf-8");
+      });
+      response.on("end", function () {
+        resolve(marked.parse(data));
+      });
+    });
+  });
+}
+
+export { router };
