674b3193f935bdbe979ff194c87ca5d2ac24ac20
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2023-11-17 12:04:25 +0700

    change: remove unused variables

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 #!/usr/bin/env nodejs
 import cookieParser from "cookie-parser";
-import createDOMPurify from "dompurify";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import favicon from "serve-favicon";
 import helmet from "helmet";
 import mongoDBSanitize from "express-mongo-sanitize";
 import mongoose from "mongoose";
 import rateLimit from "express-rate-limit";
-import _ from "lodash";
-import { JSDOM } from "jsdom";
 import path from "path";
 import dotenv from "dotenv";
 const cors = require("cors");
@@ -17,10 +14,6 @@ const corsOptions = {
   credentials: true
 };
 dotenv.config({ path: path.join(__dirname, "../credentials/.env") });
-
-const window: any = new JSDOM("").window;
-const DOMPurify = createDOMPurify(window);
-
 import { addLogMessageMetadata, LogMessageLevel } from "./server/core/log";
 import { getLicenses } from "./server/core/licenses";
 
@@ -114,29 +107,31 @@ app.use(cookieParser());
 // Routes
 require("fs")
   .readdirSync(require("path").join(__dirname, "./server/routes"))
-  .forEach((file: any) => {
+  .forEach((file: string) => {
     app.use(require("./server/routes/" + file).router);
   });
 
 require("fs")
   .readdirSync(require("path").join(__dirname, "./server/api"))
-  .forEach((file: any) => {
+  .forEach((file: string) => {
     app.use(require("./server/api/" + file).router);
   });
 
 // PUT THIS LAST (404 page)
-app.get("*", function (request: any, response: any) {
+app.get("*", function (request: Request, response: Response) {
   response
     .status(404)
     .render(__dirname + "/server/views/pages/404", { resourceName: "page" });
 });
 
 // stuff that needs to be at the end
-app.use((error: any, request: any, response: any, next: any) => {
-  console.error(addLogMessageMetadata(error.stack, LogMessageLevel.ERROR));
-  response.status(500);
-  response.render(__dirname + "/server/views/pages/error.ejs");
-});
+app.use(
+  (error: any, request: Request, response: Response, next: NextFunction) => {
+    console.error(addLogMessageMetadata(error.stack, LogMessageLevel.ERROR));
+    response.status(500);
+    response.render(__dirname + "/server/views/pages/error.ejs");
+  }
+);
 
 // load licenses
 async function loadLicenses() {
