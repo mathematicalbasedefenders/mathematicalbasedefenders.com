0d900049a9c02a547c246429f7ba3bd968217c10
mistertfy64 <thathobotfy64@gmail.com>
2024-04-20 16:38:47 +0700

    change: add custom typings

---
+++ src/server/typings/MembershipInterface.ts
@@ -0,0 +1,11 @@
+interface MembershipInterface {
+  isDeveloper: boolean;
+  isAdministrator: boolean;
+  isModerator: boolean;
+  isContributor: boolean;
+  isTester: boolean;
+  isDonator: boolean;
+  specialRank: string;
+}
+
+export { MembershipInterface };
