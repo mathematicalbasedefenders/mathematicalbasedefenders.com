58055506933fc068798d42339befc3334cbd9bcc
mistertfy64 <mistertfy64@mistertfy64.com>
2024-10-21 15:19:02 +0700

    change: follow best practices(?) from coderabbitai

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 var router = express.Router();
 
 import rateLimit from "express-rate-limit";
+import { log } from "../core/log";
 const VERSION_URL = "https://storage.mistertfy64.com/mbd-versions.json";
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -9,21 +10,6 @@ const limiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false
 });
-
-const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
 
 const PLACEHOLDER_VERSION_DATA = {
   "game": {
@@ -40,31 +26,37 @@ const PLACEHOLDER_VERSION_DATA = {
 
 function formatDate(ISODate: string) {
   const dateObject = new Date(ISODate);
-  const m = MONTHS[dateObject.getMonth()];
-  const d = dateObject.getDate();
-  const y = dateObject.getFullYear();
-  return `${m} ${d}, ${y}`;
+  return dateObject.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
 }
 
 async function getVersions() {
-  const result = PLACEHOLDER_VERSION_DATA;
+  const result = JSON.parse(JSON.stringify(PLACEHOLDER_VERSION_DATA));
 
-  // get the data
-  const response = await fetch(VERSION_URL);
-  if (!response.ok) {
+  try {
+    // get the data
+    const response = await fetch(VERSION_URL);
+    if (!response.ok) {
+      return PLACEHOLDER_VERSION_DATA;
+    }
+
+    // response formatting
+    const responseJSON = await response.json();
+    result.game = responseJSON.game;
+    result.website = responseJSON.website;
+
+    // format date
+    result.game.releaseDate = formatDate(result.game.released);
+    result.website.releaseDate = formatDate(result.website.released);
+
+    return result;
+  } catch (error) {
+    log.warn("Error on fetching version data.");
     return PLACEHOLDER_VERSION_DATA;
   }
-
-  // response formatting
-  const responseJSON = await response.json();
-  result.game = responseJSON.game;
-  result.website = responseJSON.website;
-
-  // format date
-  result.game.releaseDate = formatDate(result.game.released);
-  result.website.releaseDate = formatDate(result.website.released);
-
-  return result;
 }
 
 router.get("/", limiter, async (request, response) => {
