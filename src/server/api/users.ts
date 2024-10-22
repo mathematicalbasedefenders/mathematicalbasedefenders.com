b3480debd3dd2e4891018127aad722d60578f752
mistertfy64 <mistertfy64@mistertfy64.com>
2024-10-22 20:43:51 +0700

    change: moduralized functions in

--- src/server/api/users.ts
+++ src/server/api/users.ts
@@ -14,7 +14,9 @@ import { log } from "../core/log";
 import { User, UserInterface } from "../models/User";
 import { EasyModeLeaderboardsAPIResponse } from "../typings/EasyModeLeaderboardsAPIResponse";
 import { StandardModeLeaderboardsAPIResponse } from "../typings/StandardModeLeaderboardsAPIResponse";
-
+type LeaderboardsAPIResponse =
+  | EasyModeLeaderboardsAPIResponse
+  | StandardModeLeaderboardsAPIResponse;
 const usernameRegex = /[A-Za-z0-9_]{3,20}/;
 const userIDRegex = /[0-9a-f]{24}/g;
 
@@ -31,12 +33,56 @@ async function getUserData(query: string) {
     : await User.findByUsernameUsingAPI(query);
 }
 
+/**
+ * Organizes the player data (from the database) further.
+ * @param data The initial data
+ * @param host The host (base) URL. This should be given from another function.
+ */
+async function organizeData(data: UserInterface, host: string) {
+  // set placeholder values
+  data.statistics.personalBestScoreOnEasySingleplayerMode.globalRank = -1;
+  data.statistics.personalBestScoreOnStandardSingleplayerMode.globalRank = -1;
+
+  // get leaderboards rank
+  const easyLeaderboardRank = await getRankForUser(data._id, `easy`, host);
+  const standardLeaderboardRank = await getRankForUser(data._id, `easy`, host);
+
+  // add leaderboards data
+  if (easyLeaderboardRank !== null) {
+    data.statistics.personalBestScoreOnEasySingleplayerMode.globalRank =
+      easyLeaderboardRank;
+  }
+  if (standardLeaderboardRank !== null) {
+    data.statistics.personalBestScoreOnStandardSingleplayerMode.globalRank =
+      standardLeaderboardRank;
+  }
+}
+
+/**
+ * Returns whether the rank position that a user is in a mode and the rank (if exists).
+ * @param userID The User ID to find rank of
+ * @param mode The gamemode to find rank on
+ * @param host The host (base) url. This should be given from another function.
+ * @returns Rank number if record exists in leaderboard (top N), null otherwise.
+ */
+async function getRankForUser(userID: string, mode: string, host: string) {
+  const response = await fetch(`${host}/api/leaderboards/${mode}`);
+  const data: Array<LeaderboardsAPIResponse> = await response.json();
+  const rank = data.findIndex(
+    (r: LeaderboardsAPIResponse) => r.playerID.toString() === userID
+  );
+  return rank === -1 ? null : rank + 1;
+}
+
 router.get("/api/users/:user", limiter, async (request, response) => {
+  // check if user is actually specified
   if (!request?.params?.user) {
     log.info(`Invalid User Request: Missing user parameter.`);
     response.status(400).json("Invalid Request.");
     return;
   }
+
+  // get user data
   const user: any = request.params.user;
   const sanitized: string = mongoDBSanitize.sanitize(user) as string;
   const host = `${request.protocol}://${request.get("Host")}`;
@@ -45,6 +91,7 @@ router.get("/api/users/:user", limiter, async (request, response) => {
     response.status(400).json("Invalid Request.");
     return;
   }
+
   // get data
   const data: UserInterface = _.cloneDeep(await getUserData(sanitized));
   if (!data) {
@@ -52,38 +99,8 @@ router.get("/api/users/:user", limiter, async (request, response) => {
     return;
   }
 
-  // set placeholder values
-  data.statistics.personalBestScoreOnEasySingleplayerMode.globalRank = -1;
-  data.statistics.personalBestScoreOnStandardSingleplayerMode.globalRank = -1;
-
-  // get easy leaderboard data
-  const easyLeaderboardResponse = await fetch(`${host}/api/leaderboards/easy`);
-  const easyLeaderboardData: Array<EasyModeLeaderboardsAPIResponse> =
-    await easyLeaderboardResponse.json();
-  const easyLeaderboardDataRank = easyLeaderboardData.findIndex(
-    (record: EasyModeLeaderboardsAPIResponse) =>
-      data._id.toString() === record.playerID.toString()
-  );
-
-  // get standard leaderboards data
-  const standardLeaderboardResponse = await fetch(
-    `${host}/api/leaderboards/standard`
-  );
-  const standardLeaderboardData: Array<StandardModeLeaderboardsAPIResponse> =
-    await standardLeaderboardResponse.json();
-  const standardLeaderboardDataRank = standardLeaderboardData.findIndex(
-    (record: StandardModeLeaderboardsAPIResponse) =>
-      data._id.toString() === record.playerID.toString()
-  );
-  // add leaderboards data
-  if (easyLeaderboardDataRank !== -1) {
-    data.statistics.personalBestScoreOnEasySingleplayerMode.globalRank =
-      easyLeaderboardDataRank + 1;
-  }
-  if (standardLeaderboardDataRank !== -1) {
-    data.statistics.personalBestScoreOnStandardSingleplayerMode.globalRank =
-      standardLeaderboardDataRank + 1;
-  }
+  // organize data
+  organizeData(data, host);
   // send data
   response.status(200).json(data);
 });
