15953e6ab5da2e05002d440b1e03e059f4b86720
mistertfy64 <mistertfy64@mistertfy64.com>
2024-10-26 23:13:59 +0700

    change: add ms to time function in format number

--- src/server/core/format-number.ts
+++ src/server/core/format-number.ts
@@ -79,4 +79,15 @@ function convertToRelativeTime(x: number, countYears: boolean) {
   return duration;
 }
 
-export { addCommasToNumber, formatToRelativeTime };
+function millisecondsToTime(milliseconds: number) {
+  let m = Math.floor(milliseconds / 60000);
+  let s = Math.floor((milliseconds % 60000) / 1000)
+    .toString()
+    .padStart(2, "0");
+  let ms = Math.floor((milliseconds % 60000) % 1000)
+    .toString()
+    .padStart(3, "0");
+  return `${m}:${s}.${ms}`;
+}
+
+export { addCommasToNumber, formatToRelativeTime, millisecondsToTime };
