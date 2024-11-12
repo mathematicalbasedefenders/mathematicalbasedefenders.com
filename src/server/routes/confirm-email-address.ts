60fb7f4f406cbbfd8e6e8bebf198b7e7ad6423c8
mistertfy64 <mistertfy64@mistertfy64.com>
2024-11-12 18:05:46 +0700

    change: refactored `confirm-email-address.ts`

--- src/server/routes/confirm-email-address.ts
+++ src/server/routes/confirm-email-address.ts
@@ -1,7 +1,7 @@
 import express from "express";
 var router = express.Router();
 
-import { PendingUser } from "../models/PendingUser.js";
+import { PendingUser, PendingUserInterface } from "../models/PendingUser.js";
 import { User, UserInterface } from "../models/User";
 import Metadata from "../models/Metadata.js";
 
@@ -23,87 +23,143 @@ const limiter = rateLimit({
 import { log } from "../core/log.js";
 
 router.get("/confirm-email-address", limiter, async (request, response) => {
-  let query: any = url.parse(request.url, true).query; // possibly none
+  const [email, code] = getDataFromQueryString(request);
 
-  let uriDecodedEmail: any = decodeURIComponent(query.email) as string;
-
-  let email: string = DOMPurify.sanitize(
-    mongoDBSanitize.sanitize(uriDecodedEmail)
-  );
-  let code: string = DOMPurify.sanitize(mongoDBSanitize.sanitize(query.code));
-
-  let pendingUserRecord = await PendingUser.findOne({
-    emailAddress: email
-  }).clone();
-
-  if (
-    pendingUserRecord &&
-    pendingUserRecord["emailConfirmationCode"] === code
-  ) {
-    try {
-      let metadataDocument = await Metadata.findOne({
-        documentIsMetadata: true
-      }).clone();
-      let stringifiedJSON = JSON.stringify(metadataDocument);
-      let object = JSON.parse(stringifiedJSON);
-      let userCount = object["usersRegistered"];
-
-      let dataToSave = {
-        username: pendingUserRecord["username"],
-        usernameInAllLowercase: pendingUserRecord["usernameInAllLowercase"],
-        emailAddress: pendingUserRecord["emailAddress"],
-        hashedPassword: pendingUserRecord["hashedPassword"],
-        userNumber: userCount + 1,
-        creationDateAndTime: Date.now(),
-        statistics: {
-          gamesPlayed: 0
-        },
-        membership: {
-          isDeveloper: false,
-          isAdministrator: false,
-          isModerator: false,
-          isContributor: false,
-          isTester: true,
-          isDonator: false,
-          specialRank: ""
-        }
-      };
-
-      let userModelToSave = new User(dataToSave);
-
-      await Metadata.findOneAndUpdate(
-        { documentIsMetadata: true },
-        { $inc: { usersRegistered: 1 } },
-        { returnOriginal: false, new: true }
-      ).clone();
-      log.info("There are now " + (userCount + 1) + " users registered.");
-      await userModelToSave.save();
-
-      log.info(`User ${pendingUserRecord["username"]} validated!`);
-      PendingUser.deleteOne({ emailAddress: email }, (error) => {
-        if (error instanceof Error) {
-          log.error(error.stack);
-        } else {
-          log.error(`Unknown pending user validation error: ${error}`);
-        }
-      });
-      response.redirect("/?verifiedemail=true");
-
-      return;
-    } catch (error) {
-      if (error instanceof Error) {
-        log.error(error.stack);
-      } else {
-        log.error(`Unknown license error: ${error}`);
-      }
-      response.redirect("/?erroroccurred=true");
-      return;
-    }
-  } else {
-    log.error(`No user with verification code ${code} found!`);
-    response.redirect("/?erroroccurred=true");
+  if (!email || !code) {
+    log.warn(`Invalid e-mail and/or code supplied while verifying! (${code})`);
+    response.redirect("/?verified=false");
     return;
   }
+
+  const pendingUserRecord = await getPendingUser(email, code);
+
+  if (!pendingUserRecord) {
+    log.warn(`Pending user record not found! (${code})`);
+    response.redirect("/?verified=false");
+    return;
+  }
+
+  try {
+    // get user count
+    const userCount = await getUserCount();
+
+    // create user
+    const dataToSave = createUserObject(pendingUserRecord, userCount);
+
+    // save user
+    const userModelToSave = new User(dataToSave);
+    await userModelToSave.save();
+    log.info(`User ${pendingUserRecord["username"]} validated!`);
+
+    // delete pending user record
+    await deletePendingUserRecord(email);
+
+    // update metadata
+    await updateMetadata();
+  } catch (error) {
+    if (error instanceof Error) {
+      log.error("Error while user validation:");
+      log.error(error.stack);
+    } else {
+      log.error(`Unknown user validation error: ${error}`);
+    }
+    response.redirect("/?verified=false");
+  }
 });
+
+function getDataFromQueryString(request: any) {
+  const query: any = url.parse(request.url, true).query; // possibly none
+  if (query == null) {
+    return [null, null];
+  }
+  // get email
+  const uriDecodedEmail: any = decodeURIComponent(query.email) as string;
+  // sanitize email
+  const email: string =
+    DOMPurify.sanitize(mongoDBSanitize.sanitize(uriDecodedEmail)) ?? null;
+  // get code
+  const code: string =
+    DOMPurify.sanitize(mongoDBSanitize.sanitize(query.code)) ?? null;
+  return [email, code];
+}
+
+async function getPendingUser(email: string, code: string) {
+  const user = await PendingUser.findOne({
+    $and: [{ emailAddress: email }, { code: code }]
+  }).clone();
+  return user;
+}
+
+/**
+ * Gets the total user count from the database's metadata document.
+ * FIXME: This may not be 100% accurate.
+ * Additionally, this makes 1 extra DB request.
+ */
+async function getUserCount() {
+  const metadataDocument = await Metadata.findOne({
+    documentIsMetadata: true
+  }).clone();
+  const stringifiedJSON = JSON.stringify(metadataDocument);
+  const object = JSON.parse(stringifiedJSON);
+  const userCount = object["usersRegistered"];
+  return userCount;
+}
+
+async function deletePendingUserRecord(email: string) {
+  PendingUser.deleteOne({ emailAddress: email }, (error) => {
+    if (error instanceof Error) {
+      log.error("Pending user record deletion error");
+      log.error(error.stack);
+      return false;
+    } else {
+      log.error(`Unknown pending user record deletion error: ${error}`);
+      return false;
+    }
+  });
+  log.error("Deleted pending user record (user verified).");
+  return true;
+}
+
+async function updateMetadata() {
+  const metadataDocument = await Metadata.findOne({
+    documentIsMetadata: true
+  }).clone();
+  const stringifiedJSON = JSON.stringify(metadataDocument);
+  const object = JSON.parse(stringifiedJSON);
+  const userCount = object["usersRegistered"];
+  await Metadata.findOneAndUpdate(
+    { documentIsMetadata: true },
+    { $inc: { usersRegistered: 1 } },
+    { returnOriginal: false, new: true }
+  ).clone();
+  log.info("There are now " + (userCount + 1) + " users registered.");
+}
+
+function createUserObject(
+  pendingUserRecord: PendingUserInterface,
+  userCount: number
+) {
+  const dataToSave = {
+    username: pendingUserRecord["username"],
+    usernameInAllLowercase: pendingUserRecord["usernameInAllLowercase"],
+    emailAddress: pendingUserRecord["emailAddress"],
+    hashedPassword: pendingUserRecord["hashedPassword"],
+    userNumber: userCount + 1,
+    creationDateAndTime: Date.now(),
+    statistics: {
+      gamesPlayed: 0
+    },
+    membership: {
+      isDeveloper: false,
+      isAdministrator: false,
+      isModerator: false,
+      isContributor: false,
+      isTester: true,
+      isDonator: false,
+      specialRank: ""
+    }
+  };
+  return dataToSave;
+}
 
 export { router };
