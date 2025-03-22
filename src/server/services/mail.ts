cab613548fec90dee76257de973dc75c43e1d6b3
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-03-22 18:50:42 +0700

    change: added logging to mail.ts service + deletion of pending user if mail failed to send

--- src/server/services/mail.ts
+++ src/server/services/mail.ts
@@ -1,5 +1,6 @@
 import { log } from "../core/log.js";
 import * as mail from "../core/mail.js";
+import { PendingUser } from "../models/PendingUser.js";
 
 async function sendMailToUnverifiedUser(
   desiredUsername: string,
@@ -7,6 +8,13 @@ async function sendMailToUnverifiedUser(
   confirmationCode: string
 ) {
   if (!(await mail.sendMailToNewlyRegisteredUser(email, confirmationCode))) {
+    log.error(
+      `Unable to send verification message to ${desiredUsername}'s e-mail!`
+    );
+
+    await PendingUser.deleteOne({ emailAddress: email }).clone();
+    log.info("Deleted pending user record (e-mail failed to send).");
+
     return {
       success: false,
       redirectTo: "?errorID=internalError"
