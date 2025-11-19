b90bb1c587187ac681a4ad05654a5a6d033ec348
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-19 21:44:59 +0700

    fix(api): add error handling

--- src/api/routes/metadata.ts
+++ src/api/routes/metadata.ts
@@ -2,10 +2,14 @@ import express from "express";
 import MetadataRepository from "../repositories/MetadataRepository";
 const router = express.Router();
 
-router.get("/metadata", async function (request, response) {
-  const metadataRepository = new MetadataRepository();
-  const data = await metadataRepository.getMetadata();
-  response.status(data.status).json(data);
+router.get("/metadata", async function (request, response, next) {
+  try {
+    const metadataRepository = new MetadataRepository();
+    const data = await metadataRepository.getMetadata();
+    response.status(data.status).json(data);
+  } catch (error) {
+    next(error);
+  }
   return;
 });
 
--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -2,10 +2,14 @@ import express from "express";
 import UserRepository from "../repositories/UserRepository";
 const router = express.Router();
 
-router.get("/user/:query", async function (request, response) {
-  const userRepository = new UserRepository();
-  const data = await userRepository.getUserData(request.params.query);
-  response.status(data.status).json(data);
+router.get("/user/:query", async function (request, response, next) {
+  try {
+    const userRepository = new UserRepository();
+    const data = await userRepository.getUserData(request.params.query);
+    response.status(data.status).json(data);
+  } catch (error) {
+    next(error);
+  }
   return;
 });
 
