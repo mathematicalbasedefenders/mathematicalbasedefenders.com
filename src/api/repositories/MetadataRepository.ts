a1375b8091eaa876432db3c19e29a60d94dc9e93
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-19 21:35:17 +0700

    fix(api): metadata repository

--- src/api/repositories/MetadataRepository.ts
+++ src/api/repositories/MetadataRepository.ts
@@ -4,7 +4,10 @@ import RepositoryResponse from "../types/RepositoryResponse";
 
 export default class MetadataRepository {
   async getMetadata(): Promise<RepositoryResponse> {
-    const data = await Metadata.findOne({});
+    const data = await Metadata.findOne({}).select({
+      usersRegistered: 1,
+      _id: 0
+    });
     if (!data) {
       log.warn(`Can't find Metadata from MetadataRepository`);
       return {
