8b84519daf385b727edd2268a4e3dc684e39a6ce
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-19 20:18:06 +0700

    feature: add `/user/:query` route

---
+++ src/api/routes/users.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import UserRepository from "../repositories/User";
+const router = express.Router();
+
+router.get("/user/:query", async function (request, response) {
+  const userRepository = new UserRepository();
+  const data = await userRepository.getUserData(request.params.query);
+  response.status(data.status).json(data);
+  return;
+});
+
+export { router };
