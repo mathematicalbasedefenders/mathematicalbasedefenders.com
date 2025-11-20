57646fdd950d27c081fac5469bb1aeba4124a2a9
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-11-20 08:32:03 +0700

    fix(api): fix typo in user route

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -2,7 +2,7 @@ import express from "express";
 import UserRepository from "../repositories/UserRepository";
 const router = express.Router();
 
-router.get("/user/:query", async function (request, response, next) {
+router.get("/users/:query", async function (request, response, next) {
   try {
     const userRepository = new UserRepository();
     const data = await userRepository.getUserData(request.params.query);
