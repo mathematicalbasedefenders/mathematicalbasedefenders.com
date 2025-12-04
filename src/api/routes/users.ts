2f4bbbfba462b360d5079469745931d09b0979a0
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-05 01:39:22 +0700

    feature(api): add `PATCH /users` for update password

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -2,6 +2,7 @@ import express from "express";
 import UserRepository from "../repositories/UserRepository";
 import PendingUserRepository from "../repositories/PendingUserRepository";
 import log from "../core/log";
+import PendingPasswordResetRepository from "../repositories/PendingPasswordResetRepository";
 const router = express.Router();
 
 router.get("/users/:query", async function (request, response, next) {
@@ -15,6 +16,7 @@ router.get("/users/:query", async function (request, response, next) {
   return;
 });
 
+// Verify new user
 router.put("/users", async function (request, response, next) {
   try {
     const pendingUserRepository = new PendingUserRepository();
@@ -48,6 +50,50 @@ router.put("/users", async function (request, response, next) {
       decodedEmail,
       decodedCode
     );
+    response.status(data.statusCode).json(data);
+  } catch (error) {
+    next(error);
+  }
+  return;
+});
+
+// Process reset password reset
+router.patch("/users", async function (request, response, next) {
+  try {
+    const pendingPasswordResetRepository = new PendingPasswordResetRepository();
+    const email = request.body.email;
+    const confirmationCode = request.body.code;
+    const newPassword = request.body.newPassword;
+
+    if (typeof email !== "string") {
+      log.warn(`Request to verify user failed: Invalid e-mail type.`);
+      response.status(400).json({
+        success: false,
+        error: "Invalid credentials.",
+        statusCode: 400
+      });
+      return;
+    }
+
+    if (typeof confirmationCode !== "string") {
+      log.warn(`Request to verify user failed: Invalid code type.`);
+      response.status(400).json({
+        success: false,
+        error: "Invalid credentials.",
+        statusCode: 400
+      });
+      return;
+    }
+
+    const decodedEmail = decodeURIComponent(email);
+    const decodedCode = decodeURIComponent(confirmationCode);
+
+    const data =
+      await pendingPasswordResetRepository.verifyPendingPasswordReset(
+        decodedEmail,
+        decodedCode,
+        newPassword
+      );
     response.status(data.statusCode).json(data);
   } catch (error) {
     next(error);
