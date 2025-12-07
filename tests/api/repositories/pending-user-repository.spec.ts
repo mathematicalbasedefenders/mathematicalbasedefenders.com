5e79253e5b81105055bdf8222e29d10ef94df45d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-07 16:53:07 +0700

    feature(tests): tests for `PendingUserRepository`, focusing on duplicate data (2)

--- tests/api/repositories/pending-user-repository.spec.ts
+++ tests/api/repositories/pending-user-repository.spec.ts
@@ -95,5 +95,72 @@ describe("UserRepository", function () {
       const statusCode = result.statusCode;
       statusCode.should.equal(400);
     });
+
+    it("should return status code 400 if it's a duplicate email (verified user)", async function () {
+      const data = {
+        email: "user001@example.com",
+        username: "pendingUserToCreate",
+        password: "password123"
+      };
+
+      const pendingUserRepository = new PendingUserRepository();
+      const result = await pendingUserRepository.createPendingUser(data);
+      const statusCode = result.statusCode;
+      statusCode.should.equal(400);
+    });
+
+    it("should return status code 400 if it's a duplicate username (verified user)", async function () {
+      const data = {
+        email: "PendingUserToCreate@example.com",
+        username: "User001",
+        password: "password123"
+      };
+
+      const pendingUserRepository = new PendingUserRepository();
+      const result = await pendingUserRepository.createPendingUser(data);
+      const statusCode = result.statusCode;
+      statusCode.should.equal(400);
+    });
+
+    it("should return status code 400 if it's a duplicate email (another pending user)", async function () {
+      const firstData = {
+        email: "PendingUserToCreate@example.com",
+        username: "pendingUserToCreate1",
+        password: "password123"
+      };
+
+      const secondData = {
+        email: "PendingUserToCreate@example.com",
+        username: "pendingUserToCreate2",
+        password: "password123"
+      };
+
+      const pendingUserRepository = new PendingUserRepository();
+      await pendingUserRepository.createPendingUser(firstData);
+      const result = await pendingUserRepository.createPendingUser(secondData);
+
+      const statusCode = result.statusCode;
+      statusCode.should.equal(400);
+    });
+
+    it("should return status code 400 if it's a duplicate username (another pending user)", async function () {
+      const firstData = {
+        email: "PendingUserToCreate1@example.com",
+        username: "pendingUserToCreate1",
+        password: "password123"
+      };
+
+      const secondData = {
+        email: "PendingUserToCreate2@example.com",
+        username: "pendingUserToCreate1",
+        password: "password123"
+      };
+
+      const pendingUserRepository = new PendingUserRepository();
+      await pendingUserRepository.createPendingUser(firstData);
+      const result = await pendingUserRepository.createPendingUser(secondData);
+      const statusCode = result.statusCode;
+      statusCode.should.equal(400);
+    });
   });
 });
