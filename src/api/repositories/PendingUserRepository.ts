5105f005f00ab9bf5efa6ec913a9b88d10dab1dc
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-09 15:55:30 +0700

    chore(api): not use hardcoded domain for record

--- src/api/repositories/PendingUserRepository.ts
+++ src/api/repositories/PendingUserRepository.ts
@@ -70,12 +70,17 @@ export default class PendingUserRepository {
     // here because for some reason the `+`s in an emails are replaced with spaces.
     const encodedEmail = data.email.replaceAll("+", "%2B");
 
+    const domain =
+      process.env.CREDENTIAL_SET_USED === "production"
+        ? "https://mathematicalbasedefenders.com"
+        : "http://localhost:8000";
+
     const dataToSave = {
       username: data.username,
       usernameInAllLowercase: data.username.toLowerCase(),
       emailAddress: data.email,
       hashedPassword: hashedPassword,
-      emailConfirmationLink: `https://mathematicalbasedefenders.com/confirm-email-address?email=${encodedEmail}&code=${emailConfirmationCode}`,
+      emailConfirmationLink: `${domain}/confirm-email-address?email=${encodedEmail}&code=${emailConfirmationCode}`,
       emailConfirmationCode: hashedEmailConfirmationCode,
       expiresAt: new Date(Date.now() + 1800000).getTime()
     };
