6fd4c0003ae222bbcecb023d34ade6e3f055fee3
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-12-09 15:37:17 +0700

    fix(api): add error handling to `LeaderboardsRepository`

--- src/api/repositories/LeaderboardsRepository.ts
+++ src/api/repositories/LeaderboardsRepository.ts
@@ -5,26 +5,44 @@ import RepositoryResponse from "../types/RepositoryResponse";
 
 export default class LeaderboardsRepository {
   async getEasySingleplayerLeaderboards(): Promise<RepositoryResponse> {
-    const amount = 100;
-    const data = await User.getEasySingleplayerBestScores(amount);
+    try {
+      const amount = 100;
+      const data = await User.getEasySingleplayerBestScores(amount);
 
-    log.info(`Returned Easy Mode data from LeaderboardsRepository.`);
-    return {
-      success: true,
-      statusCode: 200,
-      data: data
-    };
+      log.info(`Returned Easy Mode data from LeaderboardsRepository.`);
+      return {
+        success: true,
+        statusCode: 200,
+        data: data
+      };
+    } catch (error) {
+      log.error(`Error fetching Easy Mode leaderboards: ${error}`);
+      return {
+        success: false,
+        statusCode: 500,
+        error: "Internal Server Error."
+      };
+    }
   }
 
   async getStandardSingleplayerLeaderboards(): Promise<RepositoryResponse> {
-    const amount = 100;
-    const data = await User.getStandardSingleplayerBestScores(amount);
+    try {
+      const amount = 100;
+      const data = await User.getStandardSingleplayerBestScores(amount);
 
-    log.info(`Returned Standard Mode data from LeaderboardsRepository.`);
-    return {
-      success: true,
-      statusCode: 200,
-      data: data
-    };
+      log.info(`Returned Standard Mode data from LeaderboardsRepository.`);
+      return {
+        success: true,
+        statusCode: 200,
+        data: data
+      };
+    } catch (error) {
+      log.error(`Error fetching Standard Mode leaderboards: ${error}`);
+      return {
+        success: false,
+        statusCode: 500,
+        error: "Internal Server Error."
+      };
+    }
   }
 }
